0ffc58cd6e37bba5544a1b4c33842fb5956d4bb2
6367f766 <78221071+6367f766@users.noreply.github.com>
2025-01-20 20:06:08 +0000

    Added `zsh` command when spawning the terminal
    Signed-off-by: 6367f766 <78221071+6367f766@users.noreply.github.com>

--- config.def.h
+++ config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "zsh", NULL };  // added "zsh" command when launching st.
 static const char scratchpadname[] = "scratchpad";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 static const char *braveCmd[] = { "brave", "--window-name=scratchpad", "--window-size=\"120,34\"", NULL };
